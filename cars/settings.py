bde111f9f4dc399a86c09937d635caf2350aedf8
adityaaa29 <129036031+adityaaa29@users.noreply.github.com>
2024-02-24 09:33:54 +0530

    Update settings.py

--- cars/settings.py
+++ cars/settings.py
@@ -79,12 +79,9 @@ WSGI_APPLICATION = 'cars.wsgi.application'
 DATABASES = {
     
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dbmsmp',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': 'Jay.pethad9',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
+        
     }
 }
 import pymysql
